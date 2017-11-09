fc4b70e0ab2f9eaf698c3b808a48ec9880bc58d7
Rajesh D <dhrajesh1@gmail.com>
2017-11-09 07:14:11 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+
+app.set("port",(5000 || process.env.PORT));
+
+const cors = require('cors');
+app.use(cors());
+
+var bodyParser = require('body-parser')
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.get("/",(req,res) => {
+  res.json({"result":"success"});
+})
+
+
+app.listen(app.get('port'),function(){
+  console.log(`App running on ${app.get('port')}`);
+})
